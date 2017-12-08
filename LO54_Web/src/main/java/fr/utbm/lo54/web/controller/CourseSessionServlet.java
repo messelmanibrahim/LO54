394874803f3b2ae1253765d8e1a1cf2a2908e48b
mohamad_yassine <mod.m.yassine@gmail.com>
2017-12-08 17:48:53 +0100

    web use services

--- LO54_Web/src/main/java/fr/utbm/lo54/web/controller/CourseSessionServlet.java
+++ LO54_Web/src/main/java/fr/utbm/lo54/web/controller/CourseSessionServlet.java
@@ -8,10 +8,17 @@ package fr.utbm.lo54.web.controller;
 import fr.utbm.core.entity.Course;
 import fr.utbm.core.entity.CourseSession;
 import fr.utbm.core.entity.Location;
+import fr.utbm.core.service.CourseSessionService;
+import fr.utbm.core.service.LocationService;
+import fr.utbm.core.utils.Filter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -102,7 +109,30 @@ public class CourseSessionServlet extends HttpServlet {
 //        getServletContext().getRequestDispatcher("/WEB-INF/views/CourseSession.jsp").forward(request, response);
 /******************using Filter and services***************/
     
+        String title = request.getParameter("search_title");
+        String location = request.getParameter("search_location");
+        String date = request.getParameter("search_date");
+        Filter filter = new Filter();
+        if (title != null && !title.isEmpty()) {
+            filter.setCode(title);
+        }
+        if (location != null && !location.isEmpty()) {
+            filter.setCity(location);
+        }
+        if (date != null && !date.isEmpty()) {
+            try {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-mm-yyyy");
+                filter.setDate(simpleDateFormat.parse(date));
+            } catch (ParseException ex) {
+                Logger.getLogger(CourseSessionServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     
+        CourseSessionService courseSessionServices= new CourseSessionService();
+        LocationService locationService = new LocationService();
+        request.setAttribute("Locations", locationService.getList());
+        request.setAttribute("Courses", courseSessionServices.getList(filter));
+        getServletContext().getRequestDispatcher("/WEB-INF/views/CourseSession.jsp").forward(request, response);
     }
 
     /**
--- LO54_Web/src/main/webapp/WEB-INF/views/CourseSession.jsp
+++ LO54_Web/src/main/webapp/WEB-INF/views/CourseSession.jsp
@@ -4,7 +4,9 @@
     Author     : yassine
 --%>
 
+<%@page import="java.util.Date"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix = "fmt" uri = "http://java.sun.com/jsp/jstl/fmt" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -20,7 +22,7 @@
         <div id="search" style="width:100%; height: 100px; background-color: #00cccc;">
             <form action="CourseSession" method="get">                    
                 <br />
-                <input type="text" name="search_title" placeholder="What course are you looking for?"  autocomplete="off"/>
+                <input type="text" name="search_title" placeholder="Couse Name"  autocomplete="off"/>
                 <br />
                 <select name="search_location" >
                         <option selected="selected" name="at_date" value="">Choose a location</option>
@@ -30,16 +32,21 @@
                 </select>
                 <input type="date" name="search_date" placeholder="Date"  autocomplete="off"/>
                 <input type="submit" value="Search"/>
-        <!-- 
-        <a href="#" class="advanced_search_icon" id="advanced_search_btn"></a> -->
             </form>
         </div>
         <div id="data">
             <c:forEach items="${Courses}" var="cs">
-                <a href="SignUp?session=${cs.id}">
+                <a href="SignUp?session=${cs.id}" style="text-decoration:none; color: black">
                 <div id="CourseSession">
                     <h1>${cs.course.code}</h1>
                     <h3>${cs.course.title}</h3>
+                    <h3>Start date: 
+                        <fmt:formatDate value = "${cs.start}" pattern = "yyyy-mm-dd" />
+                    </h3>
+                    <h3>End   date:
+                        <fmt:formatDate value = "${cs.end}" pattern = "yyyy-mm-dd" />
+                    </h3>
+                    <h3>Location  : ${cs.location.city}</h3>
                 </div>
                 </a> 
             </c:forEach>
